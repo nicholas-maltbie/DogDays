ace4dd4ab57e06ad29478a4bc2c1b788510afe12
Nicholas Maltbie <nick.dmalt@gmail.com>
2017-10-07 13:38:28 -0400

    Start work for Park Scene

---
+++ Assets/Scripts/CreateCharacter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateCharacter : MonoBehaviour {
+
+	public GameObject dogPrefab;
+	public Transform spawn;
+
+	// Use this for initialization
+	void Start () {
+		if (GameObject.FindGameObjectsWithTag ("Player").Length == 0) {
+			Instantiate (dogPrefab);
+			dogPrefab.transform.position = spawn.position;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Item : Interactable {
 
 	public Sprite icon;
-	string description, name;
+	string description, itemName;
 
 	public override void Interact(GameObject actor) {
 		actor.GetComponent<Inventory> ().AddItem (this);
