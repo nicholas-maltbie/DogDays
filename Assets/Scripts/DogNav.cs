3852cc234e6a07feebf55ccaed8a451cf9ba235a
Nicholas Maltbie <nick.dmalt@gmail.com>
2017-10-07 00:48:07 -0400

    Add basic dog and movement

---
+++ Assets/Scripts/DogNav.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogNav : MonoBehaviour {
+
+	public float moveSpeed = 1.0f;
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horiz = Input.GetAxis ("Horizontal");
+		float vert = Input.GetAxis ("Vertical");
+
+		Vector3 move = new Vector3 (horiz, vert).normalized;
+
+		transform.Translate (move * Time.deltaTime * moveSpeed);
+	}
+}
