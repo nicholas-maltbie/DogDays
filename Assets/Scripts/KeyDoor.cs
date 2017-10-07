9491c668560d8f8af4237f38b34a14e2b4bc6f1d
Nicholas Maltbie <nick.dmalt@gmail.com>
2017-10-07 19:40:27 -0400

    Add Park Scene

---
+++ Assets/Scripts/KeyDoor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyDoor : Door {
+
+	public string key;
+
+	public override void Interact(GameObject actor) {
+		if (actor.GetComponent<Inventory> ().HasItemWithName (key)) {
+			base.Interact (actor);
+			actor.GetComponent<Inventory> ().RemoveItemWithName (key);
+		}
+	}
+}
