d16c061743d3332c29eb2ad66831666bdc45c3d6
Nicholas Maltbie <nick.dmalt@gmail.com>
2017-10-08 00:01:19 -0400

    Added icecream storyline

---
+++ Assets/Scripts/GiveItemInteract.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiveItemInteract : Interactable {
+
+	public bool givenItem = false;
+	public GameObject itemToGive;
+
+	public override void Interact(GameObject actor) {
+		if (!givenItem) {
+			GameObject itemToGive = Instantiate (this.itemToGive);
+			actor.GetComponent<Inventory> ().AddItem (itemToGive.GetComponent<Item> ());
+			givenItem = true;
+		}
+	}
+}
---
+++ Assets/Scripts/IceCreamInteract.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceCreamInteract : Interactable {
+
+	public string consumed;
+	public Condition satisfy;
+	public GameObject girl;
+	public Sprite girlHappy;
+
+	public override void Interact(GameObject actor) {
+		if (actor.GetComponent<Inventory> ().HasItemWithName (consumed)) {
+			Destroy (actor.GetComponent<Inventory> ().RemoveItemWithName (consumed));
+			satisfy.satisfied = true;
+			Destroy (girl.GetComponent<Condition> ());
+			Destroy (girl.GetComponent<Interactable> ());
+			girl.GetComponent<SpriteRenderer> ().sprite = girlHappy;
+		}
+	}
+}
