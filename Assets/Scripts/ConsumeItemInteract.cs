f3af29d11bd0e1c84085848aadb6fa019df3646a
Nicholas Maltbie <nick.dmalt@gmail.com>
2017-10-08 01:04:53 -0400

    Add yellow dog storyline

---
+++ Assets/Scripts/ConsumeItemInteract.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsumeItemInteract : Interactable {
+
+	public string consumed;
+	public Condition satisfy;
+
+	public override void Interact(GameObject actor) {
+		if (actor.GetComponent<Inventory> ().HasItemWithName (consumed)) {
+			Destroy (actor.GetComponent<Inventory> ().RemoveItemWithName (consumed));
+			satisfy.satisfied = true;
+		}
+	}
+}
