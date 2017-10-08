6f7de3053b69a834b1e53ff089cb68a567b2278d
K Waters <waterskm@mail.uc.edu>
2017-10-08 05:05:54 -0500

    girl dialogue complete

--- Assets/Scripts/IceCreamInteract.cs
+++ Assets/Scripts/IceCreamInteract.cs
@@ -7,15 +7,21 @@ public class IceCreamInteract : Interactable {
 	public string consumed;
 	public Condition satisfy;
 	public GameObject girl;
-	public Sprite girlHappy;
+	public Sprite girlHappy, girlHappySel;
 
 	public override void Interact(GameObject actor) {
 		if (actor.GetComponent<Inventory> ().HasItemWithName (consumed)) {
 			Destroy (actor.GetComponent<Inventory> ().RemoveItemWithName (consumed));
 			satisfy.satisfied = true;
-			Destroy (girl.GetComponent<Condition> ());
-			Destroy (girl.GetComponent<Interactable> ());
+			Destroy (girl.GetComponent< HeldItemCondition > ());
+			Destroy (girl.GetComponent< InteractTask > ());
 			girl.GetComponent<SpriteRenderer> ().sprite = girlHappy;
+			InteractionDialogue dia = girl.GetComponent < InteractionDialogue >();
+			dia.dialogue = "Thanks for the ice cream!";
+			dia.icon = girlHappy;
+			dia.normalSprite = girlHappy;
+			dia.selectedSprite = girlHappySel;
+			
 		}
 	}
 }
