62f1ca83d0bd41a1fd21979f6e75485cc293a563
Nicholas Maltbie <nick.dmalt@gmail.com>
2017-10-08 01:36:56 -0400

    Add animation storyline with boy

---
+++ Assets/Scripts/AnimationInteract.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationInteract : Interactable {
+
+	public Animation anim;
+	public AnimationClip clip;
+	public Condition satisfy;
+	public int numTriggers = 1;
+
+	public override void Interact(GameObject actor) {
+		if (numTriggers > 0) {
+			anim.clip = clip;
+			anim.Play ();
+			satisfy.satisfied = true;
+			numTriggers--;
+		}
+	}
+}
