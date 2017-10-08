d9b509bbc481c9e63133e353e13f83a45631d3db
K Waters <waterskm@mail.uc.edu>
2017-10-07 20:36:12 -0500

    start works

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour {
 
@@ -20,5 +21,10 @@ public class TitleScreen : MonoBehaviour {
 		#else
 			Application.Quit();
 		#endif
+	}
+	
+	public void Enter () {
+		string target = "Apartment";
+		SceneManager.LoadScene(target, LoadSceneMode.Single);
 	}
 }
