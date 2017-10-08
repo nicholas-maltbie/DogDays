50993d0793f79545d6b1d4031a8f98e752eea1ce
K Waters <waterskm@mail.uc.edu>
2017-10-08 01:39:46 -0500

    Pause screen - exit and menu buttons work

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void Quit () {
+		#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+		#else
+			Application.Quit();
+		#endif
+	}
+	
+	public void MainMenu () {
+		string target = "Title";
+		SceneManager.LoadScene(target, LoadSceneMode.Single);
+	}
+}
